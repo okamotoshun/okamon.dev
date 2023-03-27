cb1ed69b413cb4b6863181d6eb3302ee73d1a135
shun-okamoto <okamons8811@gmail.com>
2023-03-27 20:51:32 +0900

    画像を圧縮

--- src/components/layouts/FileIcon.tsx
+++ src/components/layouts/FileIcon.tsx
@@ -1,5 +1,5 @@
 import Image from 'next/image'
 
 export const FileIcon = ({ src, height = 25, width = 25 }: { src: string; height?: number; width?: number }) => {
-  return <Image src={`/image/file/${src}.png`} height={height} width={width} alt={src} />
+  return <Image src={`/image/file/${src}.png`} height={height} width={width} alt={src} quality={30} />
 }
