92f5ad81016ad8e23589089ea3a9366d0d16e570
shun-okamoto <okamons8811@gmail.com>
2023-03-28 23:28:52 +0900

    フォーマット整え

--- src/components/layouts/FileIcon.tsx
+++ src/components/layouts/FileIcon.tsx
@@ -1,5 +1,13 @@
 import Image from 'next/image'
 
-export const FileIcon = ({ src, height = 25, width = 25 }: { src: string; height?: number; width?: number }) => {
+export const FileIcon = ({
+  src,
+  height = 25,
+  width = 25,
+}: {
+  src: string
+  height?: number
+  width?: number
+}) => {
   return <Image src={`/image/file/${src}.png`} height={height} width={width} alt={src} />
 }
--- src/components/layouts/FolderIcon.tsx
+++ src/components/layouts/FolderIcon.tsx
@@ -1,5 +1,13 @@
 import Image from 'next/image'
 
-export const FolderIcon = ({ src, height=25, width=25 }: { src: string, height?: number, width?: number }) => {
-  return <Image src={`/image/folder/${src}.png`} height={height} width={width} alt={src}/>
+export const FolderIcon = ({
+  src,
+  height = 25,
+  width = 25,
+}: {
+  src: string
+  height?: number
+  width?: number
+}) => {
+  return <Image src={`/image/folder/${src}.png`} height={height} width={width} alt={src} />
 }
