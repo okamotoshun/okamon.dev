9bd3da7972775794668a40f7d03b5558591de0b0
shun-okamoto <okamons8811@gmail.com>
2023-04-02 20:32:07 +0900

    リファクタリング

--- src/const/createBlogTree.js
+++ src/const/createBlogTree.js
@@ -5,38 +5,35 @@ require('dotenv').config()
 const getMicroCMSdata = async () => {
   const url = `https://okamon.microcms.io/api/v1/blog`
   const apiKey = process.env.API_KEY
-  const apiKeyHeaderOption = { headers: { 'X-API-KEY': apiKey } }
+  const headers = { 'X-API-KEY': apiKey }
 
-  const data = await axios.get(url, apiKeyHeaderOption).then((r) => r.data)
+  try {
+    const response = await axios.get(url, { headers })
+    const data = response.data
+    const blogPages = data.contents.reduce((result, post) => {
+      const category = post.category.name
+      const page = result.find((page) => page.category === category)
+      const route = `/blog/${category.toLowerCase()}/${post.id}`
 
-  const blogPages = data.contents.reduce((result, post) => {
-    const category = post.category.name
-    const page = result.find((page) => page.category === category)
-    if (page) {
-      page.items.push({
-        id: post.id,
-        name: post.title,
-        route: `/blog/${category.toLowerCase()}/${post.id}`,
-      })
-    } else {
-      result.push({
-        id: result.length + 6,
-        category,
-        route: `/blog/${category}`,
-        items: [
-          {
-            id: post.id,
-            name: post.title,
-            route: `/blog/${category.toLowerCase()}/${post.id}`,
-          },
-        ],
-      })
-    }
+      if (page) {
+        page.items.push({ id: post.id, name: post.title, route })
+      } else {
+        result.push({
+          id: result.length + 6,
+          category,
+          route: `/blog/${category.toLowerCase()}`,
+          items: [{ id: post.id, name: post.title, route }],
+        })
+      }
 
-    return result
-  }, [])
-  console.log(blogPages)
-  return blogPages
+      return result
+    }, [])
+
+    console.log(blogPages)
+    return blogPages
+  } catch (error) {
+    console.error(error)
+  }
 }
 
 const createJSONdata = async () => {
