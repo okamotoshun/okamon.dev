2e589cf8a0051e63e4402fb62f2739a676c01631
shun-okamoto <okamons8811@gmail.com>
2023-04-03 22:09:52 +0900

    コメントの削除

--- src/const/pages.tsx
+++ src/const/pages.tsx
@@ -2,5 +2,4 @@ export const profilePages = [
   { id: 'helloworld', name: 'HelloWorld', category: 'HelloWorld', route: '/profile/helloworld' },
   { id: 'history', name: 'History', category: 'History', route: '/profile/history' },
   { id: 'mind', name: 'Mind', category: 'Mind', route: '/profile/mind' },
-  // { id: 'skills', name: 'Skills', category: 'Skills', route: '/profile/skills' },
 ]
