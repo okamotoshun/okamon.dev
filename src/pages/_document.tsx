5c83aa37878ca5b6af0e55f72931fa2545f6ea09
shun-okamoto <okamons8811@gmail.com>
2023-05-14 11:52:51 +0900

    空のscriptを追加
    cssのチラつきを抑える

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,10 +3,11 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="ja">
-      <Head/>
+      <Head />
       <body>
         <Main />
         <NextScript />
+        <script> </script>
       </body>
     </Html>
   )
